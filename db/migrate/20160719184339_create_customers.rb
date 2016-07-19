6c88c9759e65dba4308da46c7cd9d01022567722
Timecrash <mzvelez75@gmail.com>
2016-07-19 18:43:51 +0000

    Add Customer model

---
+++ db/migrate/20160719184339_create_customers.rb
@@ -0,0 +1,12 @@
+class CreateCustomers < ActiveRecord::Migration
+  def change
+    create_table :customers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :username
+
+      t.timestamps null: false
+    end
+  end
+end
