98ec8029e413c1607fba253c070039fb14a90bf7
Timecrash <mzvelez75@gmail.com>
2016-07-20 17:31:00 +0000

    Post migration

--- db/structure.sql
+++ db/structure.sql
@@ -144,6 +144,27 @@ ALTER TABLE ONLY users
 
 
 --
+-- Name: customers_lower_email; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX customers_lower_email ON customers USING btree (lower((email)::text));
+
+
+--
+-- Name: customers_lower_first_name; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX customers_lower_first_name ON customers USING btree (lower((first_name)::text) varchar_pattern_ops);
+
+
+--
+-- Name: customers_lower_last_name; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE INDEX customers_lower_last_name ON customers USING btree (lower((last_name)::text) varchar_pattern_ops);
+
+
+--
 -- Name: index_customers_on_email; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
@@ -189,4 +210,6 @@ INSERT INTO schema_migrations (version) VALUES ('20160719163950');
 INSERT INTO schema_migrations (version) VALUES ('20160719180711');
 
 INSERT INTO schema_migrations (version) VALUES ('20160719184339');
+
+INSERT INTO schema_migrations (version) VALUES ('20160720172659');
 
