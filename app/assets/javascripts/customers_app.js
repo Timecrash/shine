3e7f5de9e7b0c27f843d420f7aff38426f589c2d
Timecrash <mzvelez75@gmail.com>
2016-07-20 20:18:56 +0000

    Change Angular search to serve up canned responses

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -3,8 +3,31 @@ var app = angular.module('customers', []);
 app.controller("CustomerSearchController", [
                "$scope",
        function($scope) {
+         $scope.customers = [];
          $scope.search = function(searchTerm) {
-           $scope.searchedFor = searchTerm;
+           $scope.customers = [
+             {
+               "first_name":"Schuyler",
+               "last_name":"Cremin",
+               "email":"giles0@macgyver.net",
+               "username":"jillian0",
+               "created_at":"2015-03-04",
+             },
+             {
+               "first_name":"Derick",
+               "last_name":"Ebert",
+               "email":"lupel@rennerfisher.org",
+               "username":"ubaldo_kaulkel",
+               "created_at":"2015-03-04",
+             },
+             {
+               "first_name":"Derick",
+               "last_name":"Johnsons",
+               "email":"dj@somewhere.org",
+               "username":"djj",
+               "created_at":"2015-03-04",
+             }
+           ]
          }
        }
 ]);
