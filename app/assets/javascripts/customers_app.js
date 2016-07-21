a1ea33a4e52dbab620b5ff61ba7fab9f82742029
Timecrash <mzvelez75@gmail.com>
2016-07-21 17:25:37 +0000

    Fix pagination

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,16 +1,28 @@
 var app = angular.module('customers', []);
 
-app.controller("CustomerSearchController", [
-               "$scope","$http",
-       function($scope , $http) {
-         $scope.customers = [];
-         $scope.search = function(searchTerm) {
-           $http.get("/customers.json", {"params": {"keywords": searchTerm}}
-                    ).then(function(response) {
-                      $scope.customers = response.data;
-                    }, function(response) {
-                      alert("There was a problem: " + response.status);
-                    });
-         }
-       }
-]);
+app.controller("CustomerSearchController", ["$scope","$http", function($scope , $http) {
+  var page = 0;
+  $scope.customers = [];
+  
+  $scope.search = function(searchTerm) {
+    $http.get("/customers.json",
+             {"params": {"keywords": searchTerm, "page": page}}
+             ).then(function(response) {
+               $scope.customers = response.data;
+             }, function(response) {
+               alert("There was a problem: " + response.status);
+             });
+  }
+  
+  $scope.previousPage = function() {
+   if (page > 0) {
+     page = page - 1;
+     $scope.search($scope.keywords);
+   }
+  }
+  
+  $scope.nextPage = function() {
+   page += 1;
+   $scope.search($scope.keywords);
+  }
+}]);
