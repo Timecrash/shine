6982bc4ebf31525c75b637ccf11b31c659491ff8
Timecrash <mzvelez75@gmail.com>
2016-07-21 14:47:32 +0000

    Rewrote search in Angular

--- app/assets/javascripts/customers_app.js
+++ app/assets/javascripts/customers_app.js
@@ -1,33 +1,16 @@
 var app = angular.module('customers', []);
 
 app.controller("CustomerSearchController", [
-               "$scope",
-       function($scope) {
+               "$scope","$http",
+       function($scope , $http) {
          $scope.customers = [];
          $scope.search = function(searchTerm) {
-           $scope.customers = [
-             {
-               "first_name":"Schuyler",
-               "last_name":"Cremin",
-               "email":"giles0@macgyver.net",
-               "username":"jillian0",
-               "created_at":"2015-03-04",
-             },
-             {
-               "first_name":"Derick",
-               "last_name":"Ebert",
-               "email":"lupel@rennerfisher.org",
-               "username":"ubaldo_kaulkel",
-               "created_at":"2015-03-04",
-             },
-             {
-               "first_name":"Derick",
-               "last_name":"Johnsons",
-               "email":"dj@somewhere.org",
-               "username":"djj",
-               "created_at":"2015-03-04",
-             }
-           ]
+           $http.get("/customers.json", {"params": {"keywords": searchTerm}}
+                    ).then(function(response) {
+                      $scope.customers = response.data;
+                    }, function(response) {
+                      alert("There was a problem: " + response.status);
+                    });
          }
        }
 ]);
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -13,5 +13,10 @@ class CustomersController < ApplicationController
     else
       @customers = []
     end
+    
+    respond_to do |format|
+      format.html {}
+      format.json { render json: @customers }
+    end
   end
 end
