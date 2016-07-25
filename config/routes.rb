f283bb43c4649fa61bd746a16597d4d8fd719a06
Timecrash <mzvelez75@gmail.com>
2016-07-25 19:22:42 +0000

    Set up Rails customer#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'dashboard#index'
-  resources :customers, only: [:index]
+  resources :customers, only: [:index, :show]
   get "angular_test", to: "angular_test#index"
 end
