5cb525187461762294dd63df6d23f6b70e52482a
Timecrash <mzvelez75@gmail.com>
2016-07-26 17:09:16 +0000

    Add models for address seeding

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,10 @@
 class Customer < ActiveRecord::Base
+  has_many :customers_shipping_address
+  
+  def primary_shipping_address
+    self.customers_shipping_address.find_by(primary: true).address
+  end
+  
+  has_one :customers_billing_address
+  has_one :billing_adress, through: :customers_billing_address, source: :address
 end
