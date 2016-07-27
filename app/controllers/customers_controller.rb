8128fd09da89d8c8cf26d6227330ec82aaecc6ab
Timecrash <mzvelez75@gmail.com>
2016-07-27 15:49:27 +0000

    Fix issue with upgrading Postgres

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -21,9 +21,9 @@ class CustomersController < ApplicationController
   end
   
   def show
-    customer = Customer.find(params[:id])
+    customer_detail = CustomerDetail.find(params[:id])
     respond_to do |format|
-      format.json { render json: customer }
+      format.json { render json: customer_detail }
     end
   end
 end
