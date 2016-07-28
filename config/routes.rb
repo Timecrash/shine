31b2c8b159383ca6731aeb6f9ee01e3c8eb0807a
Timecrash <mzvelez75@gmail.com>
2016-07-28 14:46:07 +0000

    Add FakeBillingController with route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'dashboard#index'
   resources :customers, only: [:index, :show]
   get "angular_test", to: "angular_test#index"
+  get "fake-billing", to: "fake_billing#show"
 end
